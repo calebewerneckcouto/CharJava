0d110102cb5afd53ca18388585830c3f5c19f63a
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-06-19 13:03:01 -0300

    Char Java

---
+++ char-java/src/caracteres/java/CharClasseJava.java
@@ -0,0 +1,39 @@
+package caracteres.java;
+
+public class CharClasseJava {
+	
+	public static void main(String[] args) {
+		/*Char representa uma letra*/
+		char sexo = 'M';
+		
+		
+		if(sexo == 'F') {
+			System.out.println("Sexo:Feminino");
+		}
+				
+			if(sexo == 'M'){
+			System.out.println("Sexo:Masculino");			
+		
+			}
+			
+	}
+	
+}
+		
+	  
+	
+	
+	
+
+	 
+
+
+
+
+       
+
+		
+		
+	
+
+
